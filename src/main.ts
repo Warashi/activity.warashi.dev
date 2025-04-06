c73202522419802e796b8377e16843557cf93fa0
Shinnosuke Sawada <shin@warashi.dev>
2025-04-06 12:11:18 +0900

    fix: font-family sans-serif
    Signed-off-by: Shinnosuke Sawada <shin@warashi.dev>

--- src/main.ts
+++ src/main.ts
@@ -60,6 +60,7 @@ export class ActivityWarashiDevMain extends LitElement {
         :host {
             display: block;
             padding: 24px;
+			font-family: sans-serif;
         }
     `;
 }
